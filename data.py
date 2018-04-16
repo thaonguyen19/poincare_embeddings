af162bd9c6438812df1a2f3d879320b78e5064b3
Thao Nguyen <thao2605@stanford.edu>
2018-04-15 17:13:35 -0700

    updated parsing fn

--- data.py
+++ data.py
@@ -12,7 +12,37 @@ import numpy as np
 import torch as th
 
 
-def parse_seperator(line, length, sep='\t'):
+def generate_pairs(package_file, sep='.'): 
+    mapping = ddict(set) #map from higher order element to the all direct children in the hierarchy
+    all_names = set()
+    duplicate = set()
+
+    with open(package_file, 'r') as f:
+        for line in f:
+            package_names = line.strip().split(sep)
+            if len(package_names) < 2:
+                continue
+            for i in range(len(package_names)-1):
+                high, low = package_names[i], package_names[i+1]
+                mapping[high].add(low)
+
+    with open(f'all_package_pairs.tsv', 'w') as fout:
+        for k, v_set in mapping.items():
+            for v in v_set:
+                old_len = len(all_names)
+                all_names.add(v)
+                new_len = len(all_names)
+                if old_len == new_len: #duplicate element
+                    duplicate.add(v)
+                fout.write(f'{v}\t{k}\n') #more specific package comes first
+
+    with open(f'duplicate_packages.tsv', 'w') as fdup:
+        for i in duplicate:
+            fdup.write(f'{i}\n')
+
+
+def parse_line(line, length=2, sep='\t'):
+    #each line is either (head, tail) or (head, tail, weight). Return tuple of (head, tail, weight)
     d = line.strip().split(sep)
     if len(d) == length:
         w = 1
@@ -22,14 +52,6 @@ def parse_seperator(line, length, sep='\t'):
     else:
         raise RuntimeError(f'Malformed input ({line.strip()})')
     return tuple(d) + (w,)
-
-
-def parse_tsv(line, length=2):
-    return parse_seperator(line, length, '\t')
-
-
-def parse_space(line, length=2):
-    return parse_seperator(line, length, ' ')
 
 
 def iter_line(fname, fparse, length=2, comment='#'):
@@ -50,20 +72,26 @@ def intmap_to_list(d):
     return arr
 
 
-def slurp(fin, fparse=parse_tsv, symmetrize=False):
+def slurp(fin, fparse=parse_line, symmetrize=False):
     ecount = count()
     enames = ddict(ecount.__next__)
 
     subs = []
     for i, j, w in iter_line(fin, fparse, length=2):
+        print(i, j, w)
         if i == j:
             continue
         subs.append((enames[i], enames[j], w))
         if symmetrize:
             subs.append((enames[j], enames[i], w))
-    idx = th.from_numpy(np.array(subs, dtype=np.int))
+    idx = th.from_numpy(np.array(subs, dtype=np.int)) #array of (i, j, w)
 
     # freeze defaultdicts after training data and convert to arrays
-    objects = intmap_to_list(dict(enames))
+    objects = intmap_to_list(dict(enames)) #list of all elements
     print(f'slurp: objects={len(objects)}, edges={len(idx)}')
     return idx, objects
+    
+
+if __name__ == '__main__':
+    #slurp('test.tsv')
+    generate_pairs('functions')
