f46da1a99cd0c79f72749f35ab956291a50d665d
Thao Nguyen <thao2605@stanford.edu>
2018-05-18 00:32:05 -0700

    new evaluation: only evaluate nodes on the same branch

--- evaluation.py
+++ evaluation.py
@@ -1,70 +1,77 @@
 from eval_utils import *
 import argparse
 import os
-from data import slurp
+from data import slurp, find_main_packages
 from collections import defaultdict
 from itertools import count
 import pickle
 
-val_filename = './package_renamed_wo_clique/functions_04182018_val'
-duplicate_file = './package_renamed_wo_clique/functions_04182018_duplicate_train'
-train_dset = './package_renamed_wo_clique/functions_04182018_train.tsv'
-
+#val_filename = './package_renamed_wo_clique/functions_04182018_val'
+#duplicate_file = './package_renamed_wo_clique/functions_04182018_duplicate_train'
+#train_dset = './package_renamed_wo_clique/functions_04182018_train.tsv'
+MAIN_PACKAGES = ['shutil', 'http', 'pickle', 'collections', 'bz2', 'subprocess', 'array', 'tempfile', 'glob', 'inspect', 're', 'py', 'uuid', \
+				'numpy', 'copy', '_pytest', 'os', 'functools', 'minpack', 'gzip', 'genericpath', 'matplotlib', 'sympy', 'quadpack', 'abc', \
+				'decimal', 'datetime', 'mtrand', 'tokenize', '_pickle', 'pkgutil', 'unittest', 'contextlib', 'numbers', 'sklearn', 'multiprocessing',\
+				'jinja2', 'itertools', '_io', 'pandas', 'scipy', 'threading', 'pytz', 'dateutil', 'pathlib', 'urllib', 'mmap', 'nose', 'random', 'posixpath', 'ctypes', 'distutils', 'builtins', 'textwrap']
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Eval Poincare Embeddings')
-	#parser.add_argument('-dir', help='directory', type=str)
-	#parser.add_argument('-max_epoch', help='Maximum epoch', type=int)
-	#parser.add_argument('-interval', help='Interval to evaluate', type=int)
+	parser.add_argument('-dir', help='trained model directory', type=str)
+	parser.add_argument('-val_file', help='File containing val data', type=str)
+	parser.add_argument('-dup_file', help='File containing duplicates from train set', type=str)
+	parser.add_argument('-train_file', help='File containing train data (tsv)', type=str)
+	parser.add_argument('-max_epoch', help='Maximum epoch', type=int)
+	parser.add_argument('-interval', help='Interval to evaluate', type=int)
 	opt = parser.parse_args()
-	opt.dir = '/lfs/hyperion/0/thaonguyen/poincare_embeddings/trained_model_0513/'
-	opt.max_epoch = 575
-	opt.interval = 25
-	idx, _, _ = slurp(train_dset)
-	G_train, enames_inv_train, enames_train = build_graph(train_dset)
-	ecount = count()
-	enames_val = defaultdict(ecount.__next__)
-	enames_inv_val = dict()
+	#opt.dir = '/lfs/hyperion/0/thaonguyen/poincare_embeddings/trained_model_0513/'
+	#opt.max_epoch = 575
+	#opt.interval = 25
+	#idx, _, _ = slurp(train_dset)
+	all_val_data = []
+	G_train, enames_inv_train, enames_train = build_graph(opt.train_dset)
 	with open(val_filename, 'r') as fval:
 		for line in fval:
-			last_token = output_last_token(line.strip(), duplicate_file)
-			if last_token not in enames_train:
-				print("NOT FOUND IN TRAIN:", last_token)
-				continue
-			else:
-				enames_inv_val[enames_val[last_token]] = last_token
+			tokens = line.strip().split('.')
+			first = tokens[0]
+			for i in range(len(tokens)):
+				tokens[i] = tokens[i] + '-' + first
+			tokens = ['ROOT'] + tokens
+			tokens[-1] = output_last_token(line.strip(), opt.dup_file)
+			line_idx = []
+			for i in range(len(tokens)):
+				line_idx.append(enames_train[tokens[i]])
+			all_val_data.append(line_idx)
 
-	enames_val = dict(enames_val)
-	#print(len(enames_val.values()), min(enames_val.values()), max(enames_val.values()))
-	#print(len(enames_inv_val.keys()), min(enames_inv_val.keys()), max(enames_inv_val.keys()))
-	
+	all_val_nodes = [node for sublist in all_val_data for node in sublist]
+	all_val_nodes = set(all_val_nodes)
+	root_idx = enames_train['ROOT']
+	all_val_nodes.remove(root_idx)
+	print("Number of distinct val nodes (excluding ROOT):", len(all_val_nodes))
+	MAIN_PACKAGES.sort(key = lambda s: -len(s))
+
 	shortest_path_dict_file = opt.dir + 'shortest_path_dict_eval.pkl'
 	if os.path.isfile(shortest_path_dict_file):
 		print("loading shortest path dict pickle file...")
-		shortest_path_dict_train = pickle.load(open(shortest_path_dict_file, 'rb'))
+		shortest_path_dict = pickle.load(open(shortest_path_dict_file, 'rb'))
 	else:
 		print("Constructing shortest path dict...")
-		shortest_path_dict_train = defaultdict(dict)
-		for i in range(len(enames_val)):
-			name_i = enames_inv_val[i]
-			train_idx_i = enames_train[name_i]
-			#print(train_idx_i, name_i)
-			for j in range(i+1, len(enames_val)):
-				name_j = enames_inv_val[j]
-				train_idx_j = enames_train[name_j]
-				dist_ij = nx.shortest_path_length(G_train, source=train_idx_i, target=train_idx_j)
-				if train_idx_i < train_idx_j:
-					shortest_path_dict_train[train_idx_i][train_idx_j] = dist_ij
-				else:
-					shortest_path_dict_train[train_idx_j][train_idx_i] = dist_ij
-			shortest_path_dict_train[train_idx_i][train_idx_i] = 0
-		shortest_path_dict_train = dict(shortest_path_dict_train)
-		pickle.dump(shortest_path_dict_train, open(shortest_path_dict_file, 'wb'))
-	
+		shortest_path_dict = defaultdict(dict)
+		for i in all_val_nodes:
+			for j in all_val_nodes:
+				if j <= i:
+					continue
+				if output_main_package(enames_inv_train[i], MAIN_PACKAGES) != output_main_package(enames_inv_train[j], MAIN_PACKAGES): #i and j are in different main branches
+					continue
+				dist_ij = nx.shortest_path_length(G_train, source=i, target=j)
+				shortest_path_dict[i][j] = dist_ij
+			#shortest_path_dict[train_idx_i][train_idx_i] = 0
+		shortest_path_dict = dict(shortest_path_dict)
+		pickle.dump(shortest_path_dict, open(shortest_path_dict_file, 'wb'))
+
 	for i in range(opt.interval, opt.max_epoch+1, opt.interval):
 		suffix = '_epoch_'+str(i-1)+'.pth'
 		checkpoint_file = None
-		for file in os.listdir(opt.dir):
+		for file in os.listdir(opt.dir):s
 			if suffix in file:
 				checkpoint_file = file
 				print("Found file ", file)
@@ -72,9 +79,9 @@ if __name__ == '__main__':
 		if checkpoint_file is not None:
 			out_file = checkpoint_file[:-4] + '_nn.txt'
 			checkpoint_file = opt.dir+checkpoint_file
-			#find_shortest_path(None, idx, checkpoint_file, shortest_path_dict_train, epoch=i-1)
-			if i + opt.interval > opt.max_epoch:
+			find_shortest_path(None, checkpoint_file, shortest_path_dict, epoch=i-1)
+			if i + opt.interval > opt.max_epoch: #last epoch
 				print("norm_check for epoch ", str(i))
-				norm_check(None, idx, checkpoint_file, enames_train, G_train)
+				norm_check(None, checkpoint_file, all_val_data, epoch=i-1)
 				print("find_nn for epoch ", str(i))
-				find_nn(val_filename, None, idx, checkpoint_file, enames_train, shortest_path_dict_train, out_file, duplicate_file, n_top=5, epoch=i-1)
+				#find_nn(val_filename, None, checkpoint_file, enames_train, shortest_path_dict_train, out_file, duplicate_file, n_top=5, epoch=i-1)
---
+++ evaluation_old.py
@@ -0,0 +1,80 @@
+from eval_utils import *
+import argparse
+import os
+from data import slurp
+from collections import defaultdict
+from itertools import count
+import pickle
+
+val_filename = './package_renamed_wo_clique/functions_04182018_val'
+duplicate_file = './package_renamed_wo_clique/functions_04182018_duplicate_train'
+train_dset = './package_renamed_wo_clique/functions_04182018_train.tsv'
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Eval Poincare Embeddings')
+	#parser.add_argument('-dir', help='directory', type=str)
+	#parser.add_argument('-max_epoch', help='Maximum epoch', type=int)
+	#parser.add_argument('-interval', help='Interval to evaluate', type=int)
+	opt = parser.parse_args()
+	opt.dir = '/lfs/hyperion/0/thaonguyen/poincare_embeddings/trained_model_0513/'
+	opt.max_epoch = 575
+	opt.interval = 25
+	idx, _, _ = slurp(train_dset)
+	G_train, enames_inv_train, enames_train = build_graph(train_dset)
+	ecount = count()
+	enames_val = defaultdict(ecount.__next__)
+	enames_inv_val = dict()
+	with open(val_filename, 'r') as fval:
+		for line in fval:
+			last_token = output_last_token(line.strip(), duplicate_file)
+			if last_token not in enames_train:
+				print("NOT FOUND IN TRAIN:", last_token)
+				continue
+			else:
+				enames_inv_val[enames_val[last_token]] = last_token
+
+	enames_val = dict(enames_val)
+	#print(len(enames_val.values()), min(enames_val.values()), max(enames_val.values()))
+	#print(len(enames_inv_val.keys()), min(enames_inv_val.keys()), max(enames_inv_val.keys()))
+	
+	shortest_path_dict_file = opt.dir + 'shortest_path_dict_eval.pkl'
+	if os.path.isfile(shortest_path_dict_file):
+		print("loading shortest path dict pickle file...")
+		shortest_path_dict_train = pickle.load(open(shortest_path_dict_file, 'rb'))
+	else:
+		print("Constructing shortest path dict...")
+		shortest_path_dict_train = defaultdict(dict)
+		for i in range(len(enames_val)):
+			name_i = enames_inv_val[i]
+			train_idx_i = enames_train[name_i]
+			#print(train_idx_i, name_i)
+			for j in range(i+1, len(enames_val)):
+				name_j = enames_inv_val[j]
+				train_idx_j = enames_train[name_j]
+				dist_ij = nx.shortest_path_length(G_train, source=train_idx_i, target=train_idx_j)
+				if train_idx_i < train_idx_j:
+					shortest_path_dict_train[train_idx_i][train_idx_j] = dist_ij
+				else:
+					shortest_path_dict_train[train_idx_j][train_idx_i] = dist_ij
+			shortest_path_dict_train[train_idx_i][train_idx_i] = 0
+		shortest_path_dict_train = dict(shortest_path_dict_train)
+		pickle.dump(shortest_path_dict_train, open(shortest_path_dict_file, 'wb'))
+	
+	for i in range(opt.interval, opt.max_epoch+1, opt.interval):
+		suffix = '_epoch_'+str(i-1)+'.pth'
+		checkpoint_file = None
+		for file in os.listdir(opt.dir):
+			if suffix in file:
+				checkpoint_file = file
+				print("Found file ", file)
+				break
+		if checkpoint_file is not None:
+			out_file = checkpoint_file[:-4] + '_nn.txt'
+			checkpoint_file = opt.dir+checkpoint_file
+			#find_shortest_path(None, checkpoint_file, shortest_path_dict_train, epoch=i-1)
+			if i + opt.interval > opt.max_epoch:
+				print("norm_check for epoch ", str(i))
+				norm_check(None, checkpoint_file, enames_train, G_train)
+				print("find_nn for epoch ", str(i))
+				find_nn(val_filename, None, checkpoint_file, enames_train, shortest_path_dict_train, out_file, duplicate_file, n_top=5, epoch=i-1)
